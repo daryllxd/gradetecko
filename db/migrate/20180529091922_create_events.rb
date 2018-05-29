683c30215d3cbbbdfd1f27eb156c143f3f7bca6e
Daryll Santos <daryll.santos@gmail.com>
2018-05-30 01:59:26 +0800

    Changed Event timestamp column to timestamp type from datetime

--- db/migrate/20180529091922_create_events.rb
+++ db/migrate/20180529091922_create_events.rb
@@ -5,7 +5,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.integer :object_id, null: false
       t.string :object_type, null: false
-      t.datetime :timestamp, null: false
+      t.timestamp :timestamp, null: false
       t.jsonb :payload, null: false, default: {}
 
       t.timestamps
